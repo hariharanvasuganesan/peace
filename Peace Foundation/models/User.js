28354aa4b82d6921e3974816da21e7cbfc2942e1
Hariharan Vasuganesan <H.Vasuganesan@perficient.com>
2021-09-05 23:32:00 +0530

    fontend and backend connected

---
+++ Peace Foundation/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  governmentIdName: {
+    type: String,
+    required: true
+  },
+  governmentIdNo: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
+
